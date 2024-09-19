6b617cb2bef404c278210e7ca0fc668435068d3b
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-09-19 19:29:00 -0300

    :construction: :test_tube: :wrench: [chore][test] Finalizada as configurações para criar testes unitários.

---
+++ __tests__/router/router.test.ts
@@ -0,0 +1,20 @@
+import { describe, test, expect } from "@jest/globals"
+
+describe('Route Http API', () => {
+
+    test('Get Http Route', () => {
+        expect(2 + 3).toEqual(5)
+    });
+
+    test('Put Http Route', () => {
+        expect(2 + 3).toEqual(5)
+    });
+
+    test('Post Http Route', () => {
+        expect(2 + 3).toEqual(5)
+    });
+
+    test('Delete Http Route', () => {
+        expect(2 + 3).toEqual(5)
+    });
+})
