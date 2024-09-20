bfa051bed3ec324a4908eb1281223f75207b2498
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-09-19 22:10:07 -0300

    :construction: :label: [refactor] Adicionada a interface para simular o event do AWS Lambda.

--- aws.lambda.mock.idea.ts
+++ aws.lambda.mock.idea.ts
@@ -1,6 +1,13 @@
+import {ServerResponse} from 'node:http';
+import {IncomingMessage} from 'node:http';
 import RouterMiddleware from "./RouterMiddleware";
 
-exports.handler = (event: object, context: any) => {
+interface Event {
+    request: IncomingMessage,
+    response: ServerResponse
+}
+
+exports.handler = (event: Event, context: any) => {
     const route: RouterMiddleware = new RouterMiddleware(event.request, event.response);
     route.httpApi().run();
 }
