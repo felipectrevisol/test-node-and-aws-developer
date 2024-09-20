d0d6df0b00e600946e21fa433d3db3240cd36037
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-09-19 22:21:48 -0300

    :construction: :recycle: [refactor] Simulando a maneira da AWS Lambda para se aproximar ao maáximo do desafio.

--- aws.lambda.mock.idea.ts
+++ aws.lambda.mock.idea.ts
@@ -2,12 +2,12 @@ import {ServerResponse} from 'node:http';
 import {IncomingMessage} from 'node:http';
 import RouterMiddleware from "./RouterMiddleware";
 
-interface Event {
+export interface Event {
     request: IncomingMessage,
     response: ServerResponse
 }
 
-exports.handler = (event: Event, context: any) => {
+export default function handler (event: Event, context: any) {
     const route: RouterMiddleware = new RouterMiddleware(event.request, event.response);
     route.httpApi().run();
 }
--- server.ts
+++ server.ts
@@ -2,11 +2,12 @@ import {Server} from 'node:http';
 import {createServer} from 'node:http';
 import {ServerResponse} from 'node:http';
 import {IncomingMessage} from 'node:http';
-import RouterMiddleware from "./RouterMiddleware";
+import handler, {Event} from "./aws.lambda.mock.idea";
 
 const server: Server = createServer((request: IncomingMessage, response: ServerResponse) => {
-    const route: RouterMiddleware = new RouterMiddleware(request, response);
-    route.httpApi().run();
+    const event: Event = {request: request, response: response};
+    const context: any = null;
+    handler(event, context);
 });
 
 const PORT: number = 8000;
