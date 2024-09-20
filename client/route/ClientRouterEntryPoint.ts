5c0e4f11b460355845bad62d840a8ec4ec96604b
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-09-20 15:58:09 -0300

    :construction: [style] Organizado melhor o código.

--- client/route/ClientRouterEntryPoint.ts
+++ client/route/ClientRouterEntryPoint.ts
@@ -3,8 +3,8 @@ import {ServerResponse} from 'node:http';
 import {IncomingMessage} from 'node:http';
 import EntryPoint from '../../EntryPoint';
 import GetClientRouter from "./GetClientRouter";
-import PostClientRouter from "./PostClientRouter";
 import PutClientRouter from "./PutClientRouter";
+import PostClientRouter from "./PostClientRouter";
 import DeleteClienteRouter from "./DeleteClienteRouter";
 
 export default class ClientRouterEntryPoint extends EntryPoint {
@@ -15,9 +15,9 @@ export default class ClientRouterEntryPoint extends EntryPoint {
         super();
         {
             this.starHttpGetRouters();
-            this.starHttpPutRouters();
-            this.starHttpPostRouters();
-            this.starHttpDeleteRouters();
+            this.routers.push(new PutClientRouter(this.request, this.response));
+            this.routers.push(new PostClientRouter(this.request, this.response));
+            this.routers.push(new DeleteClienteRouter(this.request, this.response));
         }
     }
 
@@ -29,21 +29,9 @@ export default class ClientRouterEntryPoint extends EntryPoint {
         this.routers.push(new GetClientRouter(this.request, this.response, "/birthday"));
     }
 
-    private starHttpPostRouters(): void {
-        this.routers.push(new PostClientRouter(this.request, this.response));
-    }
-
-    private starHttpPutRouters(): void {
-        this.routers.push(new PutClientRouter(this.request, this.response));
-    }
-
-    private starHttpDeleteRouters(): void {
-        this.routers.push(new DeleteClienteRouter(this.request, this.response));
-    }
-
     public route(): Router {
         const route: Router | undefined = this.routers.find((route: Router) =>
-            route.path.includes(this.request.url!.toString()));
+            route.path.match(this.request.url!.toString()));
         return route === undefined ? this.routers[0] : route;
     }
 }
