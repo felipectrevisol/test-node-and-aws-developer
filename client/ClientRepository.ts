03abb3b4daf2cd0f6560bed3533c20cf8f259b82
Felipe C. Trevisol <felipecantotrevisol@gmail.com>
2024-09-21 17:59:06 -0300

    :construction: :lipstick: [feat] Desenvolvendo o Repositorio de Clientes.

--- client/ClientRepository.ts
+++ client/ClientRepository.ts
@@ -1,4 +1,37 @@
+import Client from "./Client";
+
 export default class ClientRepository {
-    constructor(connection: any) {
+    
+    private repository: Client[] = [];
+
+    constructor() {
+    }
+
+    add(client: Client) {
+        this.repository.push(client);
+    }
+
+    get clients(): ReadonlyArray<Client> {
+        return this.repository;
+    }
+
+    bring(client: Client): Client | undefined {
+        const finded: Client | undefined = this.repository.find(wanted => wanted.name === client.name);
+        return finded === undefined ? undefined : finded;
+    }
+
+    alter(client: Client): ClientRepository {
+        this.delete(client);
+        return this;
+    }
+
+    toThisNewOne(client: Client): boolean {
+        this.add(client);
+        return true;
+    }
+
+    delete(client: Client): void {
+        this.repository = this.repository.filter(wanted =>
+            wanted.name !== client.name);
     }
 }
